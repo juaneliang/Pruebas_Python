26a9fbc4836e639c17e412eb7a2db1934f4b4c5c
Juan Elian Gluschancoff <juan.gluschancoff@globalprocessing.com.ar>
2022-08-30 20:47:30 -0300

    upt cl3 ejCumple

--- clase3_3_ejercicioCumpleaños.py
+++ clase3_3_ejercicioCumpleaños.py
@@ -1,19 +1,20 @@
 class Persona:
     def __init__(self, nombre, edad):
-        print("\nSe crea la persona \n")
         self.nombre = nombre
         self.edad = edad
 
-    #def crearPersona(self, nombre, edad):
-        #self.nombre = input("Ingresar nombre de persona: \n")
-        #self.edad = input("Ingresar edad de persona: \n")
-        #personaCreada = Persona(nombre, edad)
-        #return personaCreada
-    def cumpleanios(self):
-        self.edad =+ 1
+    def crearPersona(self):
+        self.nombre = input("Ingresar nombre de persona: \n")
+        self.edad = input("Ingresar edad de persona: \n")
+        personaCreada = Persona(self.nombre, self.edad)
+        return personaCreada
 
-personaCreada = Persona("juan", 23)
-personaCreada.cumpleanios()
+    def cumpleanios(self, edad):
+        self.edad += 1
+
+#personaCreada = Persona("juan", 23)
+personaCreada = Persona.crearPersona()
+personaCreada.cumpleanios(personaCreada.edad)
 #print("La persona agregada se llama: " + (personaCreada.nombre) + 
 #"\nY cumplio: " + (personaCreada.edad) + " años.\n")
-print(f"La persona agregada se llama: {personaCreada.nombre} \nY cumplio:{personaCreada.edad} años.\n")
+print(f"La persona agregada se llama: {personaCreada.nombre} \nY cumplio: {personaCreada.edad} años.\n")
