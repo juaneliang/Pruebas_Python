0bd1946a56d267ce6f02f2278553e39accba3250
Juan Elian Gluschancoff <juan.gluschancoff@globalprocessing.com.ar>
2022-08-30 01:58:24 -0300

    upt clase3

--- clase3_2_clases.py
+++ clase3_2_clases.py
@@ -1,3 +1,12 @@
 class Alumnos:
-    def __init__(self):
-        pass
+    def __init__(self, nombre, apellido, dni):
+        print("Alumno creado")
+        self.nombre = nombre
+        self.apellido = apellido
+        self.dni = dni
+
+alumno1 = Alumnos("Juan","Gluschancoff","36397169")
+print(alumno1)
+
+
+
