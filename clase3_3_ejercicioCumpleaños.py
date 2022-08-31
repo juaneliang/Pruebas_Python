da701938933f89046c43172003b06420528c3d56
Juan Elian Gluschancoff <juan.gluschancoff@globalprocessing.com.ar>
2022-08-30 21:08:58 -0300

    upt

--- clase3_3_ejercicioCumpleaños.py
+++ clase3_3_ejercicioCumpleaños.py
@@ -1,20 +1,23 @@
+from typing_extensions import Self
+
+
 class Persona:
     def __init__(self, nombre, edad):
         self.nombre = nombre
         self.edad = edad
 
-    def crearPersona(self):
-        self.nombre = input("Ingresar nombre de persona: \n")
-        self.edad = input("Ingresar edad de persona: \n")
-        personaCreada = Persona(self.nombre, self.edad)
-        return personaCreada
+    #def crearPersona(self):
+        #self.nombre = input("Ingresar nombre de persona: \n")
+        #self.edad = input("Ingresar edad de persona: \n")
+        #personaCreada = Persona(self.nombre, self.edad)
+        #return personaCreada
 
-    def cumpleanios(self, edad):
+    def cumpleanios(self):
         self.edad += 1
 
-#personaCreada = Persona("juan", 23)
-personaCreada = Persona.crearPersona()
-personaCreada.cumpleanios(personaCreada.edad)
+personaCreada = Persona("juan", 23)
+#self.personaCreada = crearPersona()
+personaCreada.cumpleanios()
 #print("La persona agregada se llama: " + (personaCreada.nombre) + 
 #"\nY cumplio: " + (personaCreada.edad) + " años.\n")
 print(f"La persona agregada se llama: {personaCreada.nombre} \nY cumplio: {personaCreada.edad} años.\n")
