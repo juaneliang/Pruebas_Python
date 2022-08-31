15eba523c2b1342cfcebd29bbadd29c061c27987
Juan Elian Gluschancoff <juan.gluschancoff@globalprocessing.com.ar>
2022-08-30 21:45:16 -0300

    upt cl3 ej7v2

--- clase3_4_ejercicioCumpleV2.py
+++ clase3_4_ejercicioCumpleV2.py
@@ -1,11 +1,18 @@
 class Persona:
     def __init__(self, nombre, edad):
+        print("\Persona creada\n")
         self.nombre = nombre
         self.edad = edad
+
+    def agregarPersona():
+        nombre = input("\nIngresar nombre de la persona: ")
+        edad = int(input("\nIngresar edad de la persona: "))
+        persona = Persona(nombre, edad)
+        return persona
 
     def cumpleanios(self):
         self.edad += 1
 
-personaCreada = Persona("juan", 23)
+personaCreada = Persona.agregarPersona()
 personaCreada.cumpleanios()
 print(f"La persona agregada se llama: {personaCreada.nombre} \nY cumplio: {personaCreada.edad} años.\n")
